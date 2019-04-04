163954689ab25646e3796099227801031e31930d
luhuiling <15652612973@163.com>
2019-04-04 10:31:49 +0800

    验证码生成成功，准备进行shiro环境配置

---
+++ src/main/java/com/luhuiling/graduation/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.luhuiling.graduation.controller;
+
+import com.google.code.kaptcha.Producer;
+import com.luhuiling.graduation.po.utils.ShiroUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/sys")
+public class LoginController {
+
+    @Autowired
+    Producer producer;
+
+    @RequestMapping("/captcha")
+    public void captcha(HttpServletResponse response, HttpServletRequest request) throws ServletException, IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        String text = producer.createText();
+        //生成图片验证码
+        BufferedImage image = producer.createImage(text);
+        //保存到shiro session
+//        ShiroUtils.setSessionAttribute("KAPTCHA_SESSION_KEY",text);
+
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image,"jpg",out);
+        out.flush();
+    }
+}
---
+++ src/main/java/com/luhuiling/graduation/po/utils/ShiroUtils.java
@@ -0,0 +1,28 @@
+package com.luhuiling.graduation.po.utils;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+public class ShiroUtils {
+    public static Subject getSubject(){
+        return SecurityUtils.getSubject();
+    }
+
+    public static Session getSession(){
+        return SecurityUtils.getSubject().getSession();
+    }
+
+    public static void setSessionAttribute(Object key,Object value){
+        getSession().setAttribute(key, value);
+    }
+
+    public static String getKaptcha(String kaptchaSessionKey) throws BDException{
+        Object kaptcha = getSession().getAttribute(kaptchaSessionKey);
+        if(kaptcha == null){
+            throw new BDException("验证码失效",106);
+        }
+        getSession().removeAttribute(kaptchaSessionKey);
+        return kaptcha.toString();
+    }
+}
